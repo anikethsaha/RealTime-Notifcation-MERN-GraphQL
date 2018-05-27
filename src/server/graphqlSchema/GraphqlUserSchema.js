8858103a0b186248b1924848821639e4f23e3495
anikethsaha <anik220798@gmail.com>
2018-05-28 00:24:13 +0530

    Update GraphqlUserSchema.js

--- src/server/graphqlSchema/GraphqlUserSchema.js
+++ src/server/graphqlSchema/GraphqlUserSchema.js
@@ -1,13 +1,55 @@
 const graphql = require('graphql');
+var _ = require('lodash');
 const  {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt ,
   GraphQLScalarType ,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
  } = graphql;
-
 const MerchantUser = require('../model/merchant.js');
+//sample data // mongodb collection comming soon
+const transactionArray = [
+  {
+    "_id":1,
+    "recieverAccountName":"easy",
+     "recieverAccountNumber ": 2039123,
+     "senderAccountNumber" : 24121212,
+     "senderAccountName" : "tom",
+     "Amount" : 50,
+     "Merchant_Mid":"9f09f38c"
+   },
+  {
+    "_id":2,
+    "recieverAccountName":"max",
+    "recieverAccountNumber ": 202323123,
+    "senderAccountNumber" : 25451212,
+    "senderAccountName" : "jerry",
+     "Amount" : 150,
+     "Merchant_Mid":"5212b7d9"
+   }
+]
+transactionArray.map((k,i) =>{
+  console.log(k , i);
+})
+const TransactionType = new GraphQLObjectType({
+  name : "Transaction",
+  fields : ()=>({
+    _id : {Types:GraphQLInt},
+    recieverAccountName : {Types : GraphQLString},
+    recieverAccountNumber : {Types : GraphQLString},
+    senderAccountNumber : {Types : GraphQLString},
+    senderAccountName : {Types : GraphQLString},
+    Amount : {Types : GraphQLInt}
+    // MerchantUserData : {
+    //   Types : UserType,
+    //   resolve(parent,args){
+    //     return MerchantUser.find(_Mid : parent._Mid).exec();
+    //   }
+    // }
+  })
+});
 
 
 const UserType = new GraphQLObjectType({
@@ -20,7 +62,7 @@ const UserType = new GraphQLObjectType({
     account_Number : {type : GraphQLInt},
     phone_no:{type : GraphQLInt},
     identification_number:{type : GraphQLInt},
-    _Mid :{type : GraphQLInt}
+    _Mid :{type : GraphQLString}
   })
 });
 const RootQuery = new GraphQLObjectType({
@@ -36,7 +78,24 @@ const RootQuery = new GraphQLObjectType({
                 return u;
               });
         }
+    },
+    transaction :{
+        type : TransactionType,
+        args : {id : { type : GraphQLString}},
+        resolve(parent,args){
+            // fetch from db here
+
+              return _.find(transactionArray,{id:args._id});
+        }
+    },
+    Users : {
+      type :  new GraphQLList(UserType),
+      resolve(parent,args){
+        console.log("inside");
+        return MerchantUser.find().exec();
+      }
     }
+
   }
 });
 module.exports = new GraphQLSchema({
