67d58dbbc1ea022f8c50273ed4ec46c69450be52
anikethsaha <anik220798@gmail.com>
2018-05-28 14:52:12 +0530

    Update merchant.js

--- src/server/model/merchant.js
+++ src/server/model/merchant.js
@@ -1,38 +1,37 @@
 const mongoose = require('mongoose');
-var MerchantUser = new mongoose.Schema({
-  _id : {
-   type : mongoose.Schema.Types.ObjectId,
-   required: true,
-   auto: true,
- },
- name : {
+const transaction = new mongoose.Schema({
+    _id : {
+     type : mongoose.Schema.Types.ObjectId,
+     required: true,
+     auto: true,
+   },
+   recieverAccountNumber : {
+    type : String,
+    required : true
+    },
+  recieverAccountName : {
    type : String,
-   required:true,
- },
- _Mid : {
-   type : String,
-   required:true,
- },
- password : {
-   type : String,
-   required:true,
-
- },
- account_Number : {
-   type : Number,
-   required : true,
- },
- phone_no : {
-   type : Number,
-   required : true,
- },
- email : {
-   type : String,
-   required : true,
- },
- identification_number : {
-   type : Number,
-   required : true,
- }
+   required : true
+   },
+   senderAccountNumber : {
+    type : String,
+    required : true
+    },
+    senderAccountNumber : {
+     type : String,
+     required : true
+     },
+   _Tid : {
+     type : String,
+     required:true,
+   },
+   _Mid : {
+     type : String,
+     required:true,
+   },
+   Amount : {
+     type:Number,
+     required : true,
+   }
 })
-module.exports = mongoose.model('MerchantUser',MerchantUser);
+module.exports =  mongoose.model('transaction',transaction);
