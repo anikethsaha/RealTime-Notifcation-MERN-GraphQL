e3d72beab46a243e6e24d8f76489b27edec75bc6
anikethsaha <anik220798@gmail.com>
2018-05-29 15:48:37 +0530

    Update center.js

--- src/common/component/center.js
+++ src/common/component/center.js
@@ -2,17 +2,33 @@ import React from "react"
 import CenterHeader from './center-header.js';
 import PreLoadDiv from './preload-content'
 import DocsContent from './docsContent'
+import TransactionContent from './TransactionContent.js'
 export default class Center extends React.Component{
-  render(){
-    return (
-      <div className="center-bar nine columns">
-            <CenterHeader />
-            <PreLoadDiv />
-            <DocsContent />
-      </div>
-    )
+  constructor(props){
+  super(props);
+  this.state ={ loading : false};
+
+  }
+  componentWillMount(){
+    this.setState({  // re-renders the component everytime setState is called
+      loading:true
+    })
+
+  }
+  componentDidMount(){
+    this.setState({ // re-renders the component everytime setState is called
+      loading:false
+    })
   }
 
+  render(){
 
+    return (
+      <div className="center-bar nine columns">
+        <CenterHeader />
+        <TransactionContent />
+      </div>
+    )
 
+    }
 }
