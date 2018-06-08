dcfa704e45d15a13daa36beff818fc01472562d4
anikethsaha <anik220798@gmail.com>
2018-06-08 22:29:25 +0530

    Delete index.js

--- ApolloServerSchema/index.js
+++
@@ -1,8 +0,0 @@
-const {makeExecutableSchema}  = require('graphql-tools');
-const typeDefs = require('./typeDef.js');
- const resolvers = require('./resolver.js');
-
-module.exports = makeExecutableSchema({
-  typeDefs,
-  resolvers
-});
